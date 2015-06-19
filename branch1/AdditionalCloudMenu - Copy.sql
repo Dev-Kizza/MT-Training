ad04d51bf277a88cf9730c247a0f89d74858d7e0
Dev-Kizza <pretty_kizz@yahoo.com>
2015-06-19 11:27:59 +0800

    test
    df

---
+++ branch1/AdditionalCloudMenu - Copy.sql
@@ -0,0 +1,44 @@
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13020','12858','KP-FSD','Connection','12','1','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13021','13020','KP-FSD','Network','121','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13022','13020','KP-FSD','Cloud','122','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13023','12829','KP-MISHELPDESK','Connection','13','1','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13024','13023','KP-MISHELPDESK','Network','131','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13025','13023','KP-MISHELPDESK','Cloud','132','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13026','12841','KP-GMO','Connection','12','1','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13027','13026','KP-GMO','Network','121','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13028','13026','KP-GMO','Cloud','122','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13029','12855','KP-CUSTOMERCARE','Connection','12','1','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13030','13029','KP-CUSTOMERCARE','Network','121','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13031','13029','KP-CUSTOMERCARE','Cloud','122','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13032','12820','KP-SECCOM','Connection','12','1','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13033','13032','KP-SECCOM','Network','121','0','0','7.28','1');
+
+INSERT INTO `sysmenu` (`MID`, `ParentID`, `RoleCode`, `DisplayName`, `MenuIndex`, `IsDropDown`, `PID`, `version`, `type`) 
+VALUES('13034','13032','KP-SECCOM','Cloud','122','0','0','7.28','1');
